deb0fa2358efe4b84ae0dd4ac168093ffe884932
SheinaAlena <mel-alena@yandex.ru>
2021-04-05 20:51:50 +0500

    first

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,32 @@
 const path = require('path');
 module.exports = {
-    entry: './src/app.js',
+    entry: path.join(__dirname, "src", "index.jsx"),
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, 'public')
     },
     module: {
-        rules: [{
-         loader: 'babel-loader',
-         test: /\.js$/,
-         exclude: /node_modules/
-        }]
-       },
+       rules: [
+           {
+               test: /\.(js|jsx)$/,
+               include: `${__dirname}/src`,
+               loader: 'babel-loader',
+               exclude: /node_modules/,
+               query: {
+                   presets: ['@babel/env', '@babel/react'],
+                   plugins: [
+                       [
+                           "@babel/plugin-proposal-class-properties",
+                           {
+                               "loose": true
+                           }
+                       ]
+                   ]
+               }
+           },
+       ],
+   },
+
        mode: 'development',
     devServer: {
         contentBase: path.join(__dirname, 'public')
